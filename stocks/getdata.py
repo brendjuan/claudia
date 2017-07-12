446b823234bc8049b9b6bb27028e70d63aad5ca2
Brendon Jackson <btj28@cornell.edu>
2017-07-12 05:19:59 -0400

    getting data now works

--- stocks/getdata.py
+++ stocks/getdata.py
@@ -3,6 +3,7 @@ import pandas_datareader.data as web
 import datetime
 from datetime import date
 from datetime import timedelta
+import pandas as pd
 
 start = datetime.datetime(2010, 1, 1)
 tempY = date.today() - timedelta(days = 1)
@@ -10,5 +11,11 @@ end   = datetime.datetime(tempY.year, tempY.month, tempY.day)
 print(start)
 print(end)
 
-f = web.DataReader('LITE', 'yahoo', start, end)
-print (f)
+data = pd.read_csv("data/nasdaq.csv", index_col=None, header=None)
+data.columns = ["Ticker"]
+for index, row in data.iterrows():
+	print(row['Ticker'])
+	comp = row['Ticker']
+	print(type(comp))
+	f = web.DataReader(comp, 'yahoo', start, end)
+	f.to_csv('data/nasdaq/'+comp+'_'+str(end)+'.csv')
