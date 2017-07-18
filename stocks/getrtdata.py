addc24ce858f8e23d243fe9158c4550335bb80a8
Brendon Jackson <btj28@cornell.edu>
2017-07-17 21:08:26 -0400

    changed get data and realtiem data

--- stocks/getrtdata.py
+++ stocks/getrtdata.py
@@ -7,11 +7,67 @@ import pandas as pd
 import time
 import sys
 
+t_delta       = 2*60
+
+hr_offset     = 4
+opening       = (9*60) + 30
+closing       = (16*60) + 10
+
 ticks         = pd.read_csv("data/nasdaq.csv", index_col=None, header=None)
 ticks.columns = ["Ticker"]
+tt            = list(ticks['Ticker'])
+i             = 0
+
+while i < len(tt):
+	if not os.path.isfile('data/nasdaq/'+tt[i]+'.csv'):
+		tt.pop(i)
+	else:
+		i += 1
+
+i_delta = 80
+x       = 0
+y       = i_delta
+
+last_t = time.time()
+
+rt_quotes = web.get_quote_google('LITE') #LOL
 
 while True:
-	for index, row in ticks.iterrows():
+
+	rightnow = datetime.datetime.now()
+	conv_rnow = (rightnow.hour + hr_offset)*60 + rightnow.minute
+	
+	while (conv_rnow < opening):
+		time.sleep(60*5)
+
+	rt_quotes = web.get_quote_google('LITE') #LOL
+
+	while (conv_rnow < closing):
+		xx = web.get_quote_google(tt[x:y])
+		rt_quotes = rt_quotes.append(xx)
+
+		x  = y
+		y += i_delta
+
+		if y > len(tt): 
+			y = len(tt)
+
+		if x == len(tt):
+			x = 0
+			y = x + i_delta
+
+			time.sleep(t_delta - (time.time() - last_t))
+
+			last_t   = time.time()
+			rightnow = datetime.datetime.now()
+
+	rt_quotes.to_csv('data/daily/'+str(datetime.datetime.now().date())+'.csv')
+	time.sleep(60*60*16)
+
+print(xx.shape)
+print(len(tt))
+'''while True:
+		
 
 		cant = 10
 		comp = row['Ticker']
@@ -24,4 +80,4 @@ while True:
 			print(type(data))
 	quit()
 		#lite2 = web.get_quote_google('LITE')
-		#print('G: ' + str(lite2))
+		#print('G: ' + str(lite2))'''
